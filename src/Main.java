71e32e48609ba0e3bb458a30a6f3c735a4814fc6
Mathias Lindblom <mathias.lindblom@trustly.se>
2013-10-07 10:34:32 +0200

    Finished

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Main {
     private static float[][] transisionMatrix;
     private static float[][] emissionMatrix;
-    private static float[][] initialStatePDMatrix;
+    private static float[]   initialStatePDVector; //Says matrix in assignment but should/could be vector;
 
 
     public static void main(String[] args) throws IOException {
@@ -39,12 +39,70 @@ public class Main {
                     matrix[i][j] = Float.parseFloat(stringArray[col * i + j + 2]);
                 }
             }
-            if(n == 0)transisionMatrix = matrix;
-            if(n == 1)emissionMatrix = matrix;
-            if(n == 2)initialStatePDMatrix = matrix;
+            if (n == 0) { transisionMatrix = matrix; }
+            if (n == 1) { emissionMatrix = matrix; }
+            if (n == 2) { initialStatePDVector = matrix[0]; }
         }
+        System.out.print(test());
+        float[] nextStateVector = computeNextStateProb(initialStatePDVector, transisionMatrix, emissionMatrix);
+        for(float test: nextStateVector){
+            System.out.print(test + " ");
+        }
+        System.out.println();
+
+        float[] emissionPVector = computeEmissionProb(emissionMatrix, nextStateVector);
+
+        StringBuilder resultBuilder = new StringBuilder();
+        resultBuilder.append("1 " + emissionPVector.length + " ");
+        for (float emissionProp : emissionPVector) {
+            resultBuilder.append(emissionProp + " ");
+        }
+        System.out.println(resultBuilder.toString().trim());
     }
 
-    public Main() {
+    private static float[] computeNextStateProb(float[] initialStatePDVector, float[][] transisionMatrix, float[][] emissionMatrix) {
+        float[] nextStateVector = new float[initialStatePDVector.length];
+        for (int transitionRow = 0; transitionRow < transisionMatrix.length; transitionRow++) {
+            for (int transitionCol = 0; transitionCol < transisionMatrix.length; transitionCol++) {
+                nextStateVector[transitionCol] += initialStatePDVector[transitionRow] * transisionMatrix[transitionRow][transitionCol];
+            }
+        }
+        return nextStateVector;
+    }
+
+    private static float[] computeEmissionProb(float[][] emissionMatrix, float[] stateVector) {
+        float[] emissionPVector = new float[emissionMatrix[0].length];
+
+        for (int emissionIndex = 0; emissionIndex < emissionPVector.length; emissionIndex++) {
+            for (int stateProbIndex = 0; stateProbIndex < stateVector.length; stateProbIndex++) {
+                emissionPVector[emissionIndex] += stateVector[stateProbIndex] * emissionMatrix[stateProbIndex][emissionIndex];
+            }
+        }
+        return emissionPVector;
+    }
+
+    public static String test(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Transission Matrix");
+        for(int row = 0; row < transisionMatrix.length; row++){
+            sb.append('\n');
+            for(int col = 0; col < transisionMatrix.length; col++){
+                sb.append(transisionMatrix[row][col]);
+                sb.append('\t');
+            }
+        }
+        sb.append('\n');
+        sb.append('\n');
+        sb.append("Emission Matrix");
+        for(int row = 0; row < emissionMatrix.length; row++){
+            sb.append('\n');
+            for(int col = 0; col < emissionMatrix[0].length; col++){
+                sb.append(emissionMatrix[row][col]);
+                sb.append('\t');
+            }
+        }
+        sb.append('\n');
+        sb.append('\n');
+        return sb.toString();
     }
 }
